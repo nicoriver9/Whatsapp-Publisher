d4c33de9b2c9ac392e948d1709b2ae160565da36
GabrielAlias <114373470+nicoriver9@users.noreply.github.com>
2025-05-07 18:25:46 -0300

    Delete src directory

--- src/whatsapp/whatsapp.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WhatsappService } from './whatsapp.service';
-import { WhatsappController } from './whatsapp.controller';
-
-@Module({
-  providers: [WhatsappService],
-  controllers: [WhatsappController]
-})
-export class WhatsappModule {}
